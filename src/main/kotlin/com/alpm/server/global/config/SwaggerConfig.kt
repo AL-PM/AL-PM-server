deba69c77889ed121133b4d4fba60f1945e406dd
rangjin <46679581+rangjin@users.noreply.github.com>
2024-03-19 11:56:26 +0900

    Swagger 추가

---
+++ src/main/kotlin/com/alpm/server/global/config/SwaggerConfig.kt
@@ -0,0 +1,25 @@
+package com.alpm.server.global.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        val securityScheme = SecurityScheme()
+            .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER).name("Authorization")
+        val securityRequirement: SecurityRequirement = SecurityRequirement().addList("bearerAuth")
+
+        return OpenAPI()
+            .components(Components().addSecuritySchemes("bearerAuth", securityScheme))
+            .security(listOf(securityRequirement))
+    }
+
+}
