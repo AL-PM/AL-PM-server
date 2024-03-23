747d4e963bf675c15dab77d05af2f67305e87107
rangjin <46679581+rangjin@users.noreply.github.com>
2024-03-24 05:16:31 +0900

    Spring Security 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
---
+++ src/main/kotlin/com/alpm/server/global/config/security/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.alpm.server.global.config.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity) = http
+        .csrf {
+            it.disable()
+        }
+        .sessionManagement {
+            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        }
+        .authorizeHttpRequests {
+            it.requestMatchers("/swagger-ui/**", "/v3/api-docs/**")
+                .permitAll()
+                .anyRequest().authenticated()
+        }
+        .build()!!
+
+}
