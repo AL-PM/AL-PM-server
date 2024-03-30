67fd25ad15ea7ef18b24efeb3a64de46b40a16db
qualificationalitated <hk23g@fsu.edu>
2024-03-31 03:16:02 +0900

    make CodegroupDto

--- src/main/kotlin/com/alpm/server/domain/codegroup/dto/CodegroupDto.kt
+++ src/main/kotlin/com/alpm/server/domain/codegroup/dto/CodegroupDto.kt
@@ -1,4 +1,30 @@
 package com.alpm.server.domain.codegroup.dto
 
-class CodegroupDto {
+import com.alpm.server.domain.algorithm.entity.Algorithm
+import com.alpm.server.domain.user.dto.OwnerDto
+import com.alpm.server.domain.user.entity.User
+import com.alpm.server.global.common.model.Language
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+class CodegroupDto(
+
+    val id: Long,
+
+    val name: String,
+
+    val referencedCount: Int,
+
+    val verified: Boolean, // Official-User 구분
+
+    val visible: Boolean,
+
+    val language: Language, //(enum - C/C++, JAVA, Python),
+
+    val owner: OwnerDto,
+
+    val groupsAlgorithms: List<Algorithm>,
+
+    val myCodeGroups: List<User>
+) {
 }
