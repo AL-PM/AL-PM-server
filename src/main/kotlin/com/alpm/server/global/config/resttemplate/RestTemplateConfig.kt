1064c198bcabb297c5d9d0df4271956b05068801
rangjin <46679581+rangjin@users.noreply.github.com>
2024-06-05 05:58:06 +0900

    Update RestTemplateConfig

--- src/main/kotlin/com/alpm/server/global/config/resttemplate/RestTemplateConfig.kt
+++ src/main/kotlin/com/alpm/server/global/config/resttemplate/RestTemplateConfig.kt
@@ -1,6 +1,7 @@
 package com.alpm.server.global.config.resttemplate
 
 import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.converter.HttpMessageConverter
@@ -9,18 +10,15 @@ import org.springframework.web.client.RestTemplate
 
 
 @Configuration
-class RestTemplateConfig (
-
-    private val objectMapper: ObjectMapper
-
-) {
+class RestTemplateConfig {
 
     @Bean
     fun restTemplate(): RestTemplate {
         val restTemplate = RestTemplate()
         val messageConverters: MutableList<HttpMessageConverter<*>> = ArrayList()
         val jsonMessageConverter = MappingJackson2HttpMessageConverter()
-        jsonMessageConverter.objectMapper = objectMapper
+        jsonMessageConverter.objectMapper = ObjectMapper()
+            .registerModule(kotlinModule())
             .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         messageConverters.add(jsonMessageConverter)
